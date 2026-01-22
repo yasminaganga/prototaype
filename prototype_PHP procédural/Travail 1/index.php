364e569e65db937d203ff1e2c0024978c8633c24
ganka <ganka@gmail.com>
2026-01-22 09:01:46 +0000

    Update

--- prototype_PHP procédural/Travail 1/index.php
+++ prototype_PHP procédural/Travail 1/index.php
@@ -1,47 +1,41 @@
 <?php
-$erreur = "";
-$resultat = "";
+$n1 = 17;
+$n2 = 10;
 
-function calc($a, $b, $op) {
-    if ($op == '+' ) return $a + $b;
-    if ($op == '-') return $a - $b;
-    if ($op == '*') return $a * $b;
-    if ($op == '/') return ($b == 0) ? "Erreur : division par zéro" : $a / $b;
-}
+$operations = ["+", "-", "*", "/"];
 
-
-if (!empty($_POST)) {
-
-    $n1 = $_POST["n1"];
-    $n2 = $_POST["n2"];
-    $op = $_POST["op"];
-
-    if ($n1 === "" || $n2 === "") {
-        $erreur = "Veuillez remplir tous les champs.";
-    } elseif (!is_numeric($n1) || !is_numeric($n2)) {
-        $erreur = "Veuillez entrer des nombres.";
-    } else {
-        $res = calc($n1, $n2, $op);
-        $resultat = is_numeric($res) ? "$n1 $op $n2 = $res" : $res;
+function calculer($n1, $n2, $operation){
+    switch ($operation) {
+        case "+": return $n1 + $n2;
+        case "-": return $n1 - $n2;
+        case "*": return $n1 * $n2;
+        case "/":
+            if ($n2 == 0) {
+                return "Erreur : division par zéro";
+            }
+            return $n1 / $n2;
+        default:
+            return "Erreur : opération invalide";
     }
 }
-?>
 
+foreach ($operations as $op) {
 
-<form method="post">
-    Nombre 1 : <input type="text" name="n1"><br><br>
-    Nombre 2 : <input type="text" name="n2"><br><br>
-    Opération :
-    <select name="op">
-        <option>+</option>
-        <option>-</option>
-        <option>*</option>
-        <option>/</option>
-    </select><br><br>
-    <button>Calculer</button>
-</form>
+ 
+    switch ($op) {
+        case "+": $typeOp = "Addition"; break;
+        case "-": $typeOp = "Soustraction"; break;
+        case "*": $typeOp = "Multiplication"; break;
+        case "/": $typeOp = "Division"; break;
+        default: $typeOp = "Opération inconnue";
+    }
 
-<?php
-if ($erreur) echo "<p style='color:red'>$erreur</p>";
-if ($resultat) echo "<p style='color::green'>$resultat</p>";
+    $resultat = calculer($n1, $n2, $op);
+
+    echo "Nombre 1 : $n1 <br>";
+    echo "Nombre 2 : $n2 <br>";
+    echo "Type d'opération : $typeOp ($op) <br>";
+    echo "Résultat : $resultat <br>";
+    echo "<br>";
+}
 ?>
