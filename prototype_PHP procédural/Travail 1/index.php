37bd31e819db038516d1d049bc00b44dca2aeaa3
ganka <ganka@gmail.com>
2026-01-22 11:54:47 +0000

    update

--- prototype_PHP procédural/Travail 1/index.php
+++ prototype_PHP procédural/Travail 1/index.php
@@ -1,41 +1,47 @@
 <?php
-$n1 = 17;
-$n2 = 10;
+$erreur = "";
+$resultat = "";
+function calc($a, $b, $op) {
+    if ($op == '+' )
+         return $a + $b;
+    if ($op == '-') 
+        return $a - $b;
+    if ($op == '*')
+         return $a * $b;
+    if ($op == '/') 
+        return ($b == 0) ? "Erreur : division by zero" : $a / $b;
+}
 
-$operations = ["+", "-", "*", "/"];
+if(!empty($_POST)){
+    $n1 = $_POST["n1"];
+    $n2 = $_POST["n2"];
+    $op = $_POST["op"];
 
-function calculer($n1, $n2, $operation){
-    switch ($operation) {
-        case "+": return $n1 + $n2;
-        case "-": return $n1 - $n2;
-        case "*": return $n1 * $n2;
-        case "/":
-            if ($n2 == 0) {
-                return "Erreur : division par zéro";
-            }
-            return $n1 / $n2;
-        default:
-            return "Erreur : opération invalide";
+    $erreur = (!is_numeric($n1) || !is_numeric($n2)) ? 
+    "Please enter numbers." : "";
+    if(!$erreur){
+     $resultat = calc($n1, $n2, $op);
     }
 }
 
-foreach ($operations as $op) {
-
- 
-    switch ($op) {
-        case "+": $typeOp = "Addition"; break;
-        case "-": $typeOp = "Soustraction"; break;
-        case "*": $typeOp = "Multiplication"; break;
-        case "/": $typeOp = "Division"; break;
-        default: $typeOp = "Opération inconnue";
-    }
-
-    $resultat = calculer($n1, $n2, $op);
-
-    echo "Nombre 1 : $n1 <br>";
-    echo "Nombre 2 : $n2 <br>";
-    echo "Type d'opération : $typeOp ($op) <br>";
-    echo "Résultat : $resultat <br>";
-    echo "<br>";
-}
 ?>
+
+ <form method="post">
+     Nombre 1 : <input type="text" name="n1"><br><br>
+     Nombre 2 : <input type="text" name="n2"><br><br>
+     Opération :
+     <select name="op">
+         <option>+</option>
+         <option>-</option>
+         <option>*</option>
+         <option>/</option>
+     </select><br><br>
+    <button>Calculer</button>
+ </form>
+
+<?php
+if ($erreur) echo "<p style='color:red'>$erreur</p>";
+if ($resultat) echo "<p style='color:green'>$resultat</p>";
+?>
+
+
